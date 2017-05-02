5fd1afaed0d51fce5711254796f5d2b29a869fe8
Ryan Boyce <ryanjosephboyce@gmail.com>
2017-05-01 21:05:31 -0300

    Finished and tested. Hoping for the best!

--- routes/productSearch.js
+++ routes/productSearch.js
@@ -1,93 +1,115 @@
 var async = require('async');
 var request = require('request');
+
+// Build the queries here to keep the rest of the code clean, and to make it easy to update if needed.
+var walmartAPIKey = `rm25tyum3p9jm9x9x7zxshfa`;
+var walmartQueryStart =`http://api.walmartlabs.com/v1/search?apiKey=${walmartAPIKey}&query=`;
+var walmartQueryEnd = `&sort=price&order=asc`;
+
 var bestBuyAPIKey = `pfe9fpy68yg28hvvma49sc89`;
 var bestBuyQueryStart=`https://api.bestbuy.com/v1/products(name=`;
-var bestBuyQueryEnd = `*)?apiKey=${bestBuyAPIKey}&sort=salePrice.asc&format=json`
+var bestBuyQueryEnd = `*)?apiKey=${bestBuyAPIKey}&sort=salePrice.asc&format=json`;
 
-var walmartAPIKey = `rm25tyum3p9jm9x9x7zxshfa`;
-var walmartQueryStart=`http://api.walmartlabs.com/v1/search?apiKey=${walmartAPIKey}`;
-
+// Searches for the lowest priced item from either BestBuy or WalMart that contains the 
+// strings passed into the request. The more precise you are with your terms, the better.
+// WalMart searches are very, very loose and. BestBuy searches were EXACT match of entire 
+// title until adding a wildcard into the search. Was unsure which to use. Put in wildcard
+// to make it more useful.
 exports.findLowestPriceByProductName = function(req, res) {
-	var walmartQuery = walmartQueryStart + `&query=${req.query.name}&sort=price&order=asc`;
 	var walmartItem;
 	var walmartPrice;
 	var walmartError;
 
-	var bestBuyQuery = bestBuyQueryStart + req.query.name + bestBuyQueryEnd;
 	var bestBuyItem;
 	var bestBuyPrice;
 	var bestBuyError;
+
+	// Must be called asynchronously or else you'll just get empty results.
 	async.parallel([
-	function(callback) {
-		// setTimeout(function() {
+		// Call to WalMart API.
+		function(callback) {
+			// Build the Query.
+			var walmartQuery = walmartQueryStart + req.query.name + walmartQueryEnd;
+
+			// Execute the request.
 			request(walmartQuery, function (error, response, body) {
 				if (!error && response.statusCode == 200) {
-					walmartResponse = JSON.parse(body); // Print the response
+					walmartResponse = JSON.parse(body); 
+			
+					// WalMart gives a message instead of returning a null result for 'items'.
 					if(walmartResponse.message !== "Results not found!") {
 						walmartItem = walmartResponse.items[0];
 						walmartPrice = walmartItem.salePrice;
-						console.log(walmartPrice);
-						callback(false, walmartPrice)
+						callback(false, walmartPrice);
 					}else{
-						console.log("No WalMart Results");
 						walmartError = true;
-						callback(false, null)
+						callback(false, null);
 					}
 				}
 			})
-		// }, 5000);
-	},
+		},
+		
+		// Call to BestBuy API.
+		function(callback) {
+			// Build the Query.
+			var bestBuyQuery = bestBuyQueryStart + req.query.name + bestBuyQueryEnd;
 
-	function(callback) {
-		// Build the Query
-		// setTimeout(function() {
+			// Execute the request.
 			request(bestBuyQuery, function (error, response, body) {
 				if (!error && response.statusCode == 200) {
-					bestBuyResponse = JSON.parse(body); // Print the response
+					bestBuyResponse = JSON.parse(body);
 					bestBuyItem = bestBuyResponse.products[0];
 					if(bestBuyItem != null) {
 						bestBuyPrice = bestBuyItem.salePrice;
-						console.log(bestBuyPrice);
-						callback(false, bestBuyPrice)
+						callback(false, bestBuyPrice);
 					} else {
-						console.log("No BestBuy Results");
 						bestBuyError = true;
-						callback(false, null)
+						callback(false, null);
 					}
 				}
 			})
-		// }, 5000);
-	}
-	],
-	
-	function(err, results) {
-		if(walmartError === true && bestBuyError === true) {
-			console.log("Both Errored");
-			return res.json({Error: "No Results Found"});
-		} else if (walmartError === true) {
-			console.log("Walmart Error");
-			return res.json({bestBuySalePrice: bestBuyPrice});
-		} else if (bestBuyError === true) {
-			console.log("BestBuy Error");
-			return res.json({walmartSalePrice: walmartPrice});
-		} else {
-			console.log("Both returned");
-			if (bestBuyPrice > walmartPrice) {
-				return res.json({walmartSalePrice: walmartPrice});
+		}
+	], // End parallel function call.
+		
+		function(err, results) {
+			if(walmartError === true && bestBuyError === true) {
+				// Give generic error to check for if neither site gives a result.
+				return res.json({Error: "No Results Found"});
+			} else if (walmartError === true) {
+				// Automatically return BestBuy result if no WalMart Result
+				return res.json({
+					name: bestBuyItem.name,
+					price: bestBuyPrice,
+					currency: "CAD",
+					location: "BestBuy"
+				});
+			} else if (bestBuyError === true) {
+				// Automatically return WalMart result if no BestBuy Result
+				return res.json({
+					name: walmartItem.name,
+					price: walmartPrice,
+					currency: "CAD",
+					location: "WalMart"
+				});
 			} else {
-				return res.json({bestBuySalePrice: bestBuyPrice});
+				// Compare the prices if both returned result. 
+				// Flipped a coin for equal prices: BestBuy wins a tie.
+				if (bestBuyPrice > walmartPrice) {
+					return res.json({
+						name: walmartItem.name,
+						price: walmartPrice,
+						currency: "CAD",
+						location: "WalMart"
+					});
+				} else {
+					return res.json({
+						name: bestBuyItem.name,
+						price: bestBuyPrice,
+						currency: "CAD",
+						location: "BestBuy"
+					});
+				}
 			}
 		}
-	}
 	);
 }
-
-exports.getBestBuyKey = function(req, res) {
-	res.send(bestBuyAPIKey)
-}
-
-exports.getWalMartKey = function(req, res) {
-	res.send(walmartAPIKey)
-}
-
-// Apple iPod Touch 4th Generation
--- server.js
+++ server.js
@@ -1,23 +1,18 @@
-var http = require('http');
 var express = require('express');
 var apiCalls = require('./routes/productSearch')
 
-var app = express();
+var express = express();
 
 // Simple display message for root directory.
-app.get('/', function (req, res) {
-  res.send('This is the dev test for Ryan Boyce');
+express.get('/', function (req, res) {
+  res.send('This is the Blue Spurs programming test for Ryan Boyce. Written 2017-05-01');
 })
 
 // Meat of the program goes here.
-app.get('/product/search', apiCalls.findLowestPriceByProductName)
+express.get('/product/search', apiCalls.findLowestPriceByProductName)
 
-// Test Functions to ensure keys are populating correctly.
-app.get('/bestBuyKey', apiCalls.getBestBuyKey)
-app.get('/walmartKey', apiCalls.getWalMartKey)
-
-// Chose port 8081 to listen on. 
-app.listen(8081);
+// Chose port 8081 to listen on. Figured it was a safe one to use.
+express.listen(8081);
 
 // Message to display as a sanity check to show user server is running.
 console.log('Currently running on port 8081!')
